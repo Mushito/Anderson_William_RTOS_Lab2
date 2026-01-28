ecc3a13f0511ba347c73082061b89b15cc98b36c
Will Anderson <wian8501@colorado.edu>
2026-01-28 10:30:48 -0700

    Lab start ready.

---
+++ Core/Inc/Application_Code.h
@@ -0,0 +1,13 @@
+/*
+ * Application_Code.h
+ *
+ *  Created on: Jan 27, 2026
+ *      Author: willa
+ */
+
+#ifndef INC_APPLICATION_CODE_H_
+#define INC_APPLICATION_CODE_H_
+
+
+
+#endif /* INC_APPLICATION_CODE_H_ */
---
+++ Core/Src/Application_Code.c
@@ -0,0 +1,8 @@
+/*
+ * Application_Code.c
+ *
+ *  Created on: Jan 27, 2026
+ *      Author: willa
+ */
+
+#include "Application_Code.h"
