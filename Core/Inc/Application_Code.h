2620b81328501fd68d25e7433d844d63ca69d5ea
willa <wian8501@colorado.edu>
2026-01-31 20:57:56 -0700

    Application_code.c version 1 completed.

--- Core/Inc/Application_Code.h
+++ Core/Inc/Application_Code.h
@@ -8,16 +8,35 @@
 #ifndef INC_APPLICATION_CODE_H_
 #define INC_APPLICATION_CODE_H_
 
+#include <stm32f4xx_hal.h>
+#include <stm32f4xx.h>
+#include <Gyro_Driver.h>
+
+#define GREEN_LED_PIN 	GPIO_PIN_13
+#define RED_LED_PIN   	GPIO_PIN_14
+#define USER_BUTTON_PIN GPIO_PIN_0
+
+#define RED_LED_PORT     GPIOG
+#define GREEN_LED_PORT   GPIOG
+#define USER_BUTTON_PORT GPIOA
+
+#define SLOW_THRESHOLD = 500.0f;
+#define FAST_THRESHOLD = 2000.0f;
+#define ZERO_THRESHOLD = 10.0f;
+
+
 typedef enum {
     GYRO_NONE = 0,   
     GYRO_SLOW_CW,
     GYRO_FAST_CW,
     GYRO_SLOW_CCW,      
-    GYRO_FAST_CCW       
+    GYRO_FAST_CCW,
+	GYRO_ERROR
 } Gyro_Direction_t;
 
 volatile uint8_t button_Pressed = 0; 
 volatile Gyro_Direction_t g_Gyro_Direction = GYRO_NONE;
+
 
 void sampleUserButton();
 
--- Core/Src/Application_Code.c
+++ Core/Src/Application_Code.c
@@ -6,6 +6,35 @@
  */
 
 #include "Application_Code.h"
-#include <stm32f4xx.h>
-#include <Gyro_Driver.h>
 
+void sampleUserButton(){
+    if  (HAL_GPIO_ReadPin(USER_BUTTON_PORT, USER_BUTTON_PIN == GPIO_PIN_SET));
+        button_Pressed = 1;
+    else button_Pressed = 0;
+}
+
+Gyro_Direction_t getGyroDirection(void){
+    float angVel = Gyro_Get_Velocity();
+
+    if (angVel > FAST_THRESHOLD)
+        return GYRO_FAST_CW;
+    else if (angVel > SLOW_THRESHOLD)
+        return GYRO_SLOW_CW;
+    else if(angVel < ZERO_THRESHOLD && angVel > -ZERO_THRESHOLD)
+        return GYRO_NONE;
+    else if (angVel < -SLOW_THRESHOLD)
+        return GYRO_SLOW_CCW;
+    else if (angVel < -FAST_THRESHOLD)
+        return GYRO_FAST_CCW;
+    else return GYRO_ERROR;
+}
+
+void drive_LED(Gyro_Direction_t dir){
+    if (button_Pressed || dir == GYRO_SLOW_CCW || dir == GYRO_FAST_CCW)
+        HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED_PIN, GPIO_PIN_SET);
+    else HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED_PIN, GPIO_PIN_RESET);
+
+    if(button_Pressed && (dir == GYRO_FAST_CW || dir == GYRO_SLOW_CW))
+        HAL_GPIO_WritePin(RED_LED_PORT, RED_LED_PIN, GPIO_PIN_SET);
+    else HAL_GPIO_WritePin(RED_LED_PORT, RED_LED_PIN, GPIO_PIN_RESET);
+}
